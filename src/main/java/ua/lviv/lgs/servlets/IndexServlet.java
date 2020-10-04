32a72215262968e59bff570e56532d0d2446e8da
DenysKravets <denys.lviv2013@gmail.com>
2020-10-04 22:39:15 +0300

    First commit for Java_Advanced_12_maven

---
+++ src/main/java/ua/lviv/lgs/servlets/IndexServlet.java
@@ -0,0 +1,39 @@
+package ua.lviv.lgs.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+
+
+
+
+@WebServlet(urlPatterns = "/index")
+public class IndexServlet extends HttpServlet {
+	
+	/**
+	 * I have no idea why this is needed but whatever.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("at least something works!");
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+		
+	}
+	
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+
+}
